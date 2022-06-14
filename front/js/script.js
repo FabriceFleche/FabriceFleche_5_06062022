eaa519da5642ec74e6863ead377a787db5f99e93
Fabrice Fleche <fabrice.fleche@free.fr>
2022-06-14 21:27:49 +0200

    Maj JS page accueil

--- front/js/script.js
+++ front/js/script.js
@@ -12,9 +12,22 @@ fetch("http://localhost:3000/api/products")
         let elementA = document.createElement("a");
         elementA.setAttribute("href","./product.html?id="+canape._id);
         listeCanap.appendChild(elementA);
+        let article = document.createElement("article");
+        listeCanap.appendChild(article);
+        let image = document.createElement("img");
+        image.src = canape.imageUrl;
+        image.alt = canape.altTxt;
+        article.appendChild(image);
+        let titre = document.createElement("h3");
+        titre.textContent = canape.name;
+        article.appendChild(titre);
+        let description = document.createElement("p");
+        description.textContent = canape.description;
+        article.appendChild(description);
     }
   })
   .catch(function(err) {
+    console.log("erreur : " + err)
     // Une erreur est survenue
   });
 
