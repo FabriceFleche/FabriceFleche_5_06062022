f48356f0445a34cd792f41707d554a84ceecf551
Fabrice Fleche <fabrice.fleche@free.fr>
2022-06-18 20:54:54 +0200

    Creation page JS product

---
+++ front/js/product.js
@@ -0,0 +1,44 @@
+//Recuperation de l id du produit
+let idCanap = window.location.href;
+let url = new URL (idCanap);
+let refId = url.searchParams.get("id");
+
+// Recuperation de l url du produit selectione
+urlProduct = "http://localhost:3000/api/products/"+refId
+
+fetch(urlProduct)
+    .then(function(res) {
+      if (res.ok) {
+        return res.json();
+    }
+    })
+    .then ((urlProduct) => {
+      console.log(urlProduct.name);
+      document.getElementById("title").innerHTML = urlProduct.name;
+      document.getElementById("price").innerHTML = urlProduct.price;
+      document.getElementById("description").innerHTML = urlProduct.description;
+      
+      productImage = document.getElementsByClassName("item__img")[0];
+      let image = document.createElement("img");
+      image.src = urlProduct.imageUrl;
+      image.alt = urlProduct.altTxt;
+      productImage.appendChild(image);
+
+      for (let couleur of urlProduct.colors) {
+        let productColors = document.createElement("option");
+        productColors.value = couleur;
+        productColors.innerText = couleur;
+        colors.appendChild(productColors);
+    }
+    })
+    
+    .catch(function(err) {
+      console.log("erreur : " + err)
+      alert("erreur : " + err)
+    });
+
+    console.log(urlProduct);
+
+
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,8 @@
 let listeCanap = document.getElementById("items");
 
-fetch("http://localhost:3000/api/products")
+// fonction de recuperation des canapes
+function getCanapes (url) {
+fetch(url)
   .then(function(res) {
     if (res.ok) {
       return res.json();
@@ -8,28 +10,39 @@ fetch("http://localhost:3000/api/products")
   })
   .then(function(canapes) {
     console.log(canapes);
-    for (let canape of canapes) {
-        let elementA = document.createElement("a");
-        elementA.setAttribute("href","./product.html?id="+canape._id);
-        listeCanap.appendChild(elementA);
-        let article = document.createElement("article");
-        listeCanap.appendChild(article);
-        let image = document.createElement("img");
-        image.src = canape.imageUrl;
-        image.alt = canape.altTxt;
-        article.appendChild(image);
-        let titre = document.createElement("h3");
-        titre.textContent = canape.name;
-        article.appendChild(titre);
-        let description = document.createElement("p");
-        description.textContent = canape.description;
-        article.appendChild(description);
-    }
+    displayCanapes(canapes);
   })
   .catch(function(err) {
     console.log("erreur : " + err)
+    alert("erreur : " + err)
     // Une erreur est survenue
   });
+}
+  
+// fonction d'affichage
+  function displayCanapes(products) {
+    for (let canape of products) {
+      let elementA = document.createElement("a");
+      elementA.setAttribute("href","./product.html?id="+canape._id);
+              
+      let article = document.createElement("article");
+      elementA.appendChild(article);
+      
+      let image = document.createElement("img");
+      image.src = canape.imageUrl;
+      image.alt = canape.altTxt;
+      article.appendChild(image);
+      
+      let titre = document.createElement("h3");
+      titre.textContent = canape.name;
+      article.appendChild(titre);
+      
+      let description = document.createElement("p");
+      description.textContent = canape.description;
+      article.appendChild(description);
+      
+      listeCanap.appendChild(elementA);
+      }
+    }
 
-  
-  
+    getCanapes("http://localhost:3000/api/products");
