37150a575e572ab077f835eecfa9e7af8ab17385
Fabrice Fleche <fabrice.fleche@free.fr>
2022-06-19 11:58:09 +0200

    Maj recup info canape page product js

--- front/js/product.js
+++ front/js/product.js
@@ -6,6 +6,7 @@ let refId = url.searchParams.get("id");
 // Recuperation de l url du produit selectione
 urlProduct = "http://localhost:3000/api/products/"+refId
 
+//fonction de recuperation du canape selectionne
 function productUrl (url) {
 fetch(url)
     .then(function(res) {
@@ -25,28 +26,28 @@ fetch(url)
 }
 
 
-    function displayDetailCanape (urlProduct) {
-      // Test de recuperation de l url du canape
-      console.log(urlProduct.name);
-      document.getElementById("title").innerHTML = urlProduct.name;
-      document.getElementById("price").innerHTML = urlProduct.price;
-      document.getElementById("description").innerHTML = urlProduct.description;
+function displayDetailCanape (urlProduct) {
+  // Test de recuperation du nom pour le canape
+  console.log(urlProduct.name);
+  document.getElementById("title").innerHTML = urlProduct.name;
+  document.getElementById("price").innerHTML = urlProduct.price;
+  document.getElementById("description").innerHTML = urlProduct.description;
       
-      productImage = document.getElementsByClassName("item__img")[0];
-      let image = document.createElement("img");
-      image.src = urlProduct.imageUrl;
-      image.alt = urlProduct.altTxt;
-      productImage.appendChild(image);
+  productImage = document.getElementsByClassName("item__img")[0];
+  let image = document.createElement("img");
+  image.src = urlProduct.imageUrl;
+  image.alt = urlProduct.altTxt;
+  productImage.appendChild(image);
 
-      for (let couleur of urlProduct.colors) {
-        let productColors = document.createElement("option");
-        productColors.value = couleur;
-        productColors.innerText = couleur;
-        colors.appendChild(productColors);
-      }
-    }
+  for (let couleur of urlProduct.colors) {
+    let productColors = document.createElement("option");
+    productColors.value = couleur;
+    productColors.innerText = couleur;
+    colors.appendChild(productColors);
+  }
+}
 
-      productUrl("http://localhost:3000/api/products/"+refId)
+productUrl(urlProduct)
     
     
 
