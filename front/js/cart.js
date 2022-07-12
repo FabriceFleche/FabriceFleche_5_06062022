128b3eaf1cfe9d113a1d0aa73df1c4e10555411a
Fabrice Fleche <fabrice.fleche@free.fr>
2022-07-12 23:08:12 +0200

    Debut gestion btn supprimer

--- front/js/cart.js
+++ front/js/cart.js
@@ -53,10 +53,21 @@ function recupIdCanapesApi (url) {
           //affichage du tarif  
           let tarif = clone.querySelector(".cart__item__content__description > p:nth-child(3)");
           tarif.textContent = cartArticleInfo.price;
+          console.log(cartArticleInfo.price)
+
+          //Renseignement des dataSet
+          let dataSetId = cartArticleInfo._id;
+          let dataSetColor = cartArticleColors;
+          let dataId = clone.querySelector(".cart__item.dataset.id");
+          dataId = dataSetId;
+          let dataColor = clone.querySelector(".cart__item.dataset.color");
+          dataColor = dataSetColor;
+          console.log(dataId);
+          console.log(dataColor);
           
           //affichage de la photo
           productImage = clone.querySelector('.cart__item__img');
-          let image = document.createElement("img");
+          const image = productImage.querySelector("img");
           image.src = cartArticleInfo.imageUrl;
           image.alt = cartArticleInfo.altTxt;
           productImage.appendChild(image);
@@ -65,14 +76,41 @@ function recupIdCanapesApi (url) {
           let quantite = clone.querySelector('.itemQuantity');
           quantite.value = cartArticleQuantite;
 
-          //affichage de la totalQuantity
-          let quantiteTotal = document.getElementsByClassName("itemQuantity")[0].value;
-          //let totalQuantity = clone.querySelector("#totalQuantity").textContent;
-          //totalQuantity = 2;
-          console.log(quantiteTotal);
-        }
-      })
-  
+          //affichage de la Quantite totale
+         
+          totalQtePrix();
+
+          function totalQtePrix() {
+            let totalprix = 0;
+            let totalQuantite = 0;
+            let inputs = document.querySelectorAll(".itemQuantity");
+            inputs.forEach((itemQuantity, price) => {
+                let productQty = parseInt(itemQuantity.value);
+                let productPrice = parseInt(price);
+                totalQuantite += productQty;
+                let productTotalPrice = productPrice * productQty;
+                totalprix += productTotalPrice;
+                console.log(productPrice);
+            });
+            document.getElementById("totalQuantity").textContent = `${totalQuantite}`;
+            document.getElementById("totalPrice").textContent = `${totalprix}`;
+            
+          }
+       
+      // Gestion de la suppresion d'un produit
+
+          function supprimeCanap () {
+            
+          }
+
+
+      const boutonCart = document.getElementsByClassName("deleteItem");
+      //boutonCart.addEventListener("click", supprimeCanap);
+
+
+       }
+    })
+      
       .catch(function(err) {
         console.log("erreur : " + err)
         alert("erreur : " + err)
